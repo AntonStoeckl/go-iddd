12ec072c22517eb8dcc37fe35222c6d2038fa5a2
AntonStoeckl <anton@stoeckl.de>
2019-04-10 09:09:46 +0200

    add CommandHandlerWithReflection
    this is just to have it in the git history, I will not use it because
    reflection can only use exposed methods but only Handle() must be exposed

---
+++ customer/application/CommandHandlerWithReflection_test.go
@@ -0,0 +1,180 @@
+package application
+
+import (
+	"errors"
+	"go-iddd/customer/application/mocks"
+	"go-iddd/customer/model/commands"
+	"go-iddd/customer/model/valueobjects"
+	"go-iddd/shared"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestCommandHandlerWithReflectionRegister(t *testing.T) {
+	Convey("Given a CommandHandler", t, func() {
+		mockCustomers := new(mocks.Customers)
+		commandHandler := NewCommandHandlerWithReflection(mockCustomers)
+		So(commandHandler, ShouldImplement, (*shared.CommandHandler)(nil))
+
+		Convey("And given a valid Register Command", func() {
+			id := valueobjects.GenerateID()
+			emailAddress := valueobjects.NewConfirmableEmailAddress("foo@bar.com")
+			name := valueobjects.NewName("Anton", "Stöckl")
+
+			register, err := commands.NewRegister(id, emailAddress, name)
+			So(err, ShouldBeNil)
+
+			Convey("When the command is handled", func() {
+				Convey("And when applying register succeeds", func() {
+					Convey("And when saving the Customer succeeds", func() {
+						mockCustomers.On("Save", mock.AnythingOfType("*model.customer")).Return(nil).Once()
+
+						err := commandHandler.Handle(register)
+
+						Convey("Then it should register and save a Customer", func() {
+							So(err, ShouldBeNil)
+							So(mockCustomers.AssertExpectations(t), ShouldBeTrue)
+						})
+					})
+
+					Convey("And when saving the Customer fails", func() {
+						expectedErr := errors.New("mocked error")
+						mockCustomers.On("Save", mock.AnythingOfType("*model.customer")).Return(expectedErr).Once()
+
+						err := commandHandler.Handle(register)
+
+						Convey("Then it should fail", func() {
+							So(err, ShouldBeError, expectedErr)
+							So(mockCustomers.AssertExpectations(t), ShouldBeTrue)
+						})
+					})
+				})
+			})
+		})
+	})
+}
+
+func TestCommandHandlerWithReflectionConfirmEmailAddress(t *testing.T) {
+	Convey("Given a CommandHandler", t, func() {
+		mockCustomers := new(mocks.Customers)
+		commandHandler := NewCommandHandlerWithReflection(mockCustomers)
+		So(commandHandler, ShouldImplement, (*shared.CommandHandler)(nil))
+
+		Convey("And given a valid ConfirmEmailAddress Command", func() {
+			id := valueobjects.GenerateID()
+			emailAddress := valueobjects.NewConfirmableEmailAddress("foo@bar.com")
+			confirmationHash := valueobjects.GenerateConfirmationHash(emailAddress.String())
+
+			confirmEmailAddress, err := commands.NewConfirmEmailAddress(id, emailAddress, confirmationHash)
+			So(err, ShouldBeNil)
+
+			Convey("When the command is handled", func() {
+				expectedErr := errors.New("mocked error")
+
+				Convey("And when finding the Customer succeeds", func() {
+					mockCustomer := new(mocks.Customer)
+					mockCustomers.On("FindBy", id).Return(mockCustomer, nil).Once()
+
+					Convey("And when applying confirmEmailAddress succeeds", func() {
+						mockCustomer.On("Apply", confirmEmailAddress).Return(nil)
+
+						Convey("And when saving the Customer succeeds", func() {
+							mockCustomers.On("Save", mockCustomer).Return(nil).Once()
+
+							err := commandHandler.Handle(confirmEmailAddress)
+
+							Convey("Then it should confirmEmailAddress of a Customer and save it", func() {
+								So(err, ShouldBeNil)
+								So(mockCustomers.AssertExpectations(t), ShouldBeTrue)
+							})
+						})
+
+						Convey("And when saving the Customer fails", func() {
+							mockCustomers.On("Save", mockCustomer).Return(expectedErr).Once()
+
+							err := commandHandler.Handle(confirmEmailAddress)
+
+							Convey("Then it should fail", func() {
+								So(err, ShouldBeError, expectedErr)
+								So(mockCustomers.AssertExpectations(t), ShouldBeTrue)
+							})
+						})
+					})
+
+					Convey("And when applying confirmEmailAddress fails", func() {
+						mockCustomer.On("Apply", confirmEmailAddress).Return(expectedErr)
+
+						err := commandHandler.Handle(confirmEmailAddress)
+
+						Convey("Then it should fail", func() {
+							So(err, ShouldBeError, expectedErr)
+							So(mockCustomers.AssertExpectations(t), ShouldBeTrue)
+						})
+					})
+				})
+
+				Convey("And when finding the Customer fails", func() {
+					mockCustomers.On("FindBy", id).Return(nil, expectedErr).Once()
+					err := commandHandler.Handle(confirmEmailAddress)
+
+					Convey("Then it should fail", func() {
+						So(err, ShouldBeError, expectedErr)
+						So(mockCustomers.AssertExpectations(t), ShouldBeTrue)
+					})
+				})
+			})
+		})
+	})
+}
+
+func TestCommandHandlerWithReflectionInvalidCommand(t *testing.T) {
+	Convey("Given a CommandHandler", t, func() {
+		mockCustomers := new(mocks.Customers)
+		commandHandler := NewCommandHandlerWithReflection(mockCustomers)
+		So(commandHandler, ShouldImplement, (*shared.CommandHandler)(nil))
+
+		Convey("And given an invalid Command", func() {
+			var invalidCommand shared.Command
+
+			Convey("When the command is handled", func() {
+				err := commandHandler.Handle(invalidCommand)
+
+				Convey("Then it should fail", func() {
+					So(err, ShouldBeError, "commandHandler - nil command handled")
+				})
+			})
+		})
+	})
+}
+
+func TestCommandHandlerWithReflectionUnknownCommand(t *testing.T) {
+	Convey("Given a CommandHandler", t, func() {
+		mockCustomers := new(mocks.Customers)
+		commandHandler := NewCommandHandlerWithReflection(mockCustomers)
+		So(commandHandler, ShouldImplement, (*shared.CommandHandler)(nil))
+
+		Convey("And given an unknown Command", func() {
+			unknownCommand := &unknownCommand2{}
+
+			Convey("When the command is handled", func() {
+				err := commandHandler.Handle(unknownCommand)
+
+				Convey("Then it should fail", func() {
+					So(err, ShouldBeError, "commandHandler - unknown command handled")
+				})
+			})
+		})
+	})
+}
+
+type unknownCommand2 struct{}
+
+func (c *unknownCommand2) Identifier() string {
+	return "unknown"
+}
+
+func (c *unknownCommand2) CommandName() string {
+	return "unknown"
+}
