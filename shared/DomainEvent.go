7b9d31a46b6369aebb90c7c3e2aad954e09a2882
anton <anton@stoeckl.de>
2019-05-03 11:22:41 +0200

    accept string instead of aggregate factory method to avoid import cycles

--- shared/DomainEvent.go
+++ shared/DomainEvent.go
@@ -1,45 +1,68 @@
 package shared
 
 import (
+	"errors"
 	"reflect"
 	"strings"
 	"time"
 )
 
 type DomainEvent interface {
-	Identifier() AggregateIdentifier
+	Identifier() string
 	EventName() string
 	OccurredAt() string
 }
 
-type DomainEventMeta struct {
-	Identifier AggregateIdentifier
-	EventName  string
-	OccurredAt string
-}
-
-func NewDomainEventMeta(id AggregateIdentifier, aggregate Aggregate, event DomainEvent) *DomainEventMeta {
+func NewDomainEventMeta(aggregateID string, event DomainEvent, aggregateName string) *DomainEventMeta {
 	newDomainEventMeta := &DomainEventMeta{
-		Identifier: id,
-		EventName:  buildEventNameFor(aggregate, event),
+		Identifier: aggregateID,
+		EventName:  buildEventNameFor(event, aggregateName),
 		OccurredAt: time.Now().Format(time.RFC3339Nano),
 	}
 
 	return newDomainEventMeta
 }
 
-func buildEventNameFor(aggregate Aggregate, event DomainEvent) string {
+func buildEventNameFor(event DomainEvent, withAggregateName string) string {
 	eventType := reflect.TypeOf(event).String()
 	eventTypeParts := strings.Split(eventType, ".")
 	eventName := eventTypeParts[len(eventTypeParts)-1]
 	eventName = strings.Title(eventName)
 
-	aggregateType := reflect.TypeOf(aggregate).String()
-	aggregateTypeParts := strings.Split(aggregateType, ".")
-	aggregateName := aggregateTypeParts[len(aggregateTypeParts)-1]
-	aggregateName = strings.Title(aggregateName)
-
-	fullEventName := aggregateName + eventName
+	fullEventName := withAggregateName + eventName
 
 	return fullEventName
+}
+
+type DomainEventMeta struct {
+	Identifier string `json:"identifier"`
+	EventName  string `json:"eventName"`
+	OccurredAt string `json:"occurredAt"`
+}
+
+func UnmarshalDomainEventMeta(data interface{}) (*DomainEventMeta, error) {
+	values, ok := data.(map[string]interface{})
+	if !ok {
+		return nil, errors.New("zefix")
+	}
+
+	meta := &DomainEventMeta{}
+
+	for key, value := range values {
+		value, ok := value.(string)
+		if !ok {
+			return nil, errors.New("zefix")
+		}
+
+		switch key {
+		case "identifier":
+			meta.Identifier = value
+		case "eventName":
+			meta.EventName = value
+		case "occurredAt":
+			meta.OccurredAt = value
+		}
+	}
+
+	return meta, nil
 }
