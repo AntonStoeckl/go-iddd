57f91d616092c0b070d45824609b261493fc8113
anton <anton@stoeckl.de>
2019-05-10 15:37:22 +0200

    add error

--- shared/Errors.go
+++ shared/Errors.go
@@ -4,6 +4,7 @@ import "errors"
 
 var (
 	ErrInvalidInput       = errors.New("invalid input")
+	ErrNilInput           = errors.New("nil input")
 	ErrNotEqual           = errors.New("not equal")
 	ErrMarshalingFailed   = errors.New("marshaling failed")
 	ErrUnmarshalingFailed = errors.New("unmarshaling failed")
