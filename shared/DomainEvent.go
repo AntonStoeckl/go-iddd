5ab64c5a8d5e53e8a76f8c762c25ed5afa67240c
anton <anton@stoeckl.de>
2019-05-10 15:55:00 +0200

    remove helper method to assert input params

--- shared/DomainEvent.go
+++ shared/DomainEvent.go
@@ -1,35 +1,7 @@
 package shared
 
-import (
-	"fmt"
-	"reflect"
-)
-
 type DomainEvent interface {
 	Identifier() string
 	EventName() string
 	OccurredAt() string
-}
-
-func AssertEventPropertiesAreNotNilExcept(event DomainEvent, canBeNil ...string) error {
-	elem := reflect.ValueOf(event).Elem()
-	typeOf := elem.Type()
-
-outer:
-	for i := 0; i < elem.NumField(); i++ {
-		property := elem.Field(i)
-		propertyName := typeOf.Field(i).Name
-
-		for _, bar := range canBeNil {
-			if bar == propertyName {
-				continue outer
-			}
-		}
-
-		if property.IsNil() {
-			return fmt.Errorf("nil given for: %s", propertyName)
-		}
-	}
-
-	return nil
 }
