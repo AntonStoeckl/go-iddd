f0685e8a1b9b152d2804c8a2a49aa431e911b9da
anton <anton@stoeckl.de>
2019-05-23 11:35:01 +0200

    remove New() method

--- customer/domain/Customers.go
+++ customer/domain/Customers.go
@@ -5,7 +5,6 @@ import "go-iddd/customer/domain/values"
 //go:generate mockery -name Customers -output ../application/mocks -outpkg mocks -note "Regenerate by running `go generate` in customer/domain"
 
 type Customers interface {
-	New() Customer
 	Save(Customer) error
 	FindBy(id *values.ID) (Customer, error)
 }
