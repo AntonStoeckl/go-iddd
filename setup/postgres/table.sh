d0d63b237c58e8fb69efca0617f16799d5c68f2d
Anton Stöckl <anton.stoeckl@maibornwolff.de>
2019-08-30 10:18:25 +0200

    create separate DBs (with tables) for dev and test

--- setup/postgres/table.sh
+++ setup/postgres/table.sh
@@ -24,3 +24,27 @@ create index if not exists event_name_idx
 create index if not exists occurred_at_idx
 	on eventstore (occurred_at);
 EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$GOIDDD_USERNAME" "$GOIDDD_DATABASE_TEST" <<-EOSQL
+create table if not exists eventstore
+(
+	id serial not null,
+	stream_id varchar(255) not null,
+	stream_version integer default 0 not null,
+	event_name varchar(255) not null,
+	payload jsonb default '{}'::jsonb not null,
+	occurred_at timestamp with time zone not null
+);
+
+create unique index if not exists id_unique
+	on eventstore (id);
+
+create unique index if not exists stream_unique
+	on eventstore (stream_id, stream_version);
+
+create index if not exists event_name_idx
+	on eventstore (event_name);
+
+create index if not exists occurred_at_idx
+	on eventstore (occurred_at);
+EOSQL
