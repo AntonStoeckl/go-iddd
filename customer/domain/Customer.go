7487afbd1ef9896e3408c1beb24254465ceabc3b
anton <anton@stoeckl.de>
2019-10-18 21:33:47 +0200

    inline check if reconstituted customer was registered

--- customer/domain/Customer.go
+++ customer/domain/Customer.go
@@ -1,9 +1,11 @@
 package domain
 
 import (
+	"fmt"
 	"go-iddd/customer/domain/events"
 	"go-iddd/customer/domain/values"
 	"go-iddd/shared"
+	"reflect"
 
 	"github.com/cockroachdb/errors"
 )
@@ -27,13 +29,32 @@ func (customer *Customer) ID() shared.IdentifiesAggregates {
 func ReconstituteCustomerFrom(eventStream shared.DomainEvents) (*Customer, error) {
 	newCustomer := blankCustomer()
 
-	if err := eventStream.FirstEventShouldBeOfSameTypeAs(&events.Registered{}); err != nil {
+	if err := newCustomer.shouldBeRegistered(eventStream); err != nil {
 		return nil, errors.Wrap(errors.Mark(err, shared.ErrInvalidEventStream), "shared.ErrInvalidEventStream")
 	}
 
 	newCustomer.apply(eventStream...)
 
 	return newCustomer, nil
+}
+
+func (customer *Customer) shouldBeRegistered(eventStream shared.DomainEvents) error {
+	if len(eventStream) == 0 {
+		return errors.New("eventStream is empty")
+	}
+
+	expectedType := reflect.TypeOf(&events.Registered{})
+	actualType := reflect.TypeOf(eventStream[0])
+
+	if actualType != expectedType {
+		return fmt.Errorf(
+			"first event in eventStream should be [%s] but is type [%s]",
+			expectedType.String(),
+			actualType.String(),
+		)
+	}
+
+	return nil
 }
 
 func (customer *Customer) recordThat(event shared.DomainEvent) {
