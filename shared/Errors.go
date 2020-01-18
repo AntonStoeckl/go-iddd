4335490a60f6e0718ed1921f39a3ac8598000987
anton <anton@stoeckl.de>
2020-01-18 17:06:44 +0100

    remove unused error

--- shared/Errors.go
+++ shared/Errors.go
@@ -7,7 +7,6 @@ var (
 	ErrMarshalingFailed           = errors.New("marshaling failed")
 	ErrUnmarshalingFailed         = errors.New("unmarshaling failed")
 	ErrCommandIsInvalid           = errors.New("command is invalid")
-	ErrCommandIsUnknown           = errors.New("command is unknown")
 	ErrDomainConstraintsViolation = errors.New("domain constraints violation")
 	ErrConcurrencyConflict        = errors.New("concurrency conflict")
 	ErrMaxRetriesExceeded         = errors.New("max retries exceeded")
