a944aa2a8541a03c98eb95ad2ec585dbcdd6f2af
anton <anton@stoeckl.de>
2020-01-29 23:22:55 +0100

    Remove mock Command

--- service/customer/application/mocks/generateMocks.go
+++ service/customer/application/mocks/generateMocks.go
@@ -1,4 +1,3 @@
 package mocks
 
 //go:generate mockery -name ForStoringCustomers -dir ../ -output . -note "+build test"
-//go:generate mockery -name Command  -dir ../../../lib -output . -note "+build test"
