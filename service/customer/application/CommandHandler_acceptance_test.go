38cedf52105d8e453a1b54358d91211050e7a074
anton <anton@stoeckl.de>
2020-02-12 23:41:57 +0100

    Pimp acceptance test

--- service/customer/application/CommandHandler_acceptance_test.go
+++ service/customer/application/CommandHandler_acceptance_test.go
@@ -1,79 +1,72 @@
 package application_test
 
 import (
+	"database/sql"
 	"fmt"
+	"go-iddd/service/cmd"
+	"go-iddd/service/customer/application"
 	"go-iddd/service/customer/application/domain/commands"
 	"go-iddd/service/customer/application/domain/events"
 	"go-iddd/service/customer/application/domain/values"
-	"go-iddd/service/customer/infrastructure"
 	"go-iddd/service/customer/infrastructure/secondary/forstoringcustomerevents/eventstore"
 	"go-iddd/service/lib"
+	"go-iddd/service/lib/es"
+	"go-iddd/service/lib/eventstore/postgres/database"
 	"testing"
 
 	"github.com/cockroachdb/errors"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+var (
+	commandHandler                     *application.CommandHandler
+	customerEventStore                 *eventstore.CustomerEventStore
+	customerID                         values.CustomerID
+	emailAddress                       string
+	givenName                          string
+	familyName                         string
+	confirmationHash                   string
+	invalidConfirmationHash            string
+	changedEmailAddress                string
+	changedConfirmationHash            string
+	register                           commands.Register
+	confirmEmailAddress                commands.ConfirmEmailAddress
+	confirmEmailAddressWithInvalidHash commands.ConfirmEmailAddress
+	changeEmailAddress                 commands.ChangeEmailAddress
+	confirmChangedEmailAddress         commands.ConfirmEmailAddress
+)
+
 func TestCommandHandlerScenarios(t *testing.T) {
 	Convey("Customer Lifecycle Scenarios", t, func() {
-		diContainer, err := infrastructure.SetUpDIContainer()
-		So(err, ShouldBeNil)
+		var err error
 
-		commandHandler := diContainer.GetCustomerCommandHandler()
-		customerEventStore := diContainer.GetCustomerEventStore()
-
-		emailAddress := "fiona@gallagher.net"
-		givenName := "Fiona"
-		familyName := "Gallagher"
-
-		register, err := commands.NewRegister(
-			emailAddress,
-			givenName,
-			familyName,
-		)
-		So(err, ShouldBeNil)
-
-		issuedHash := register.ConfirmationHash().Hash()
+		setUpForCommandHandlerScenarios()
 
 		Convey("\nSCENARIO 1: A prospective Customer registers", func() {
 			Convey(fmt.Sprintf("When a Customer registers as [%s %s] with [%s]", givenName, familyName, emailAddress), func() {
 				err := commandHandler.Register(register)
+				So(err, ShouldBeNil)
 
 				Convey("Then she should have an unconfirmed account", func() {
-					So(err, ShouldBeNil)
-					AccountShouldBeRegisteredAndUnconfirmed(register.CustomerID(), customerEventStore)
+					ThenEventStreamShouldBe(events.Registered{})
 				})
 			})
 		})
 
 		Convey("\nSCENARIO 2: A Customer confirms her email address", func() {
 			Convey(fmt.Sprintf("Given a Customer registered as [%s %s] with [%s]", givenName, familyName, emailAddress), func() {
-				err := commandHandler.Register(register)
-				So(err, ShouldBeNil)
+				GivenCustomerRegistered()
 
-				Convey(fmt.Sprintf("and she was issued a confirmation hash [%s]", issuedHash), func() {
-					Convey(fmt.Sprintf("When she confirms her email address with confirmation hash [%s]", issuedHash), func() {
-						confirmEmailAddress, err := commands.NewConfirmEmailAddress(
-							register.CustomerID().ID(),
-							register.EmailAddress().EmailAddress(),
-							issuedHash,
-						)
-						So(err, ShouldBeNil)
-
+				Convey(fmt.Sprintf("and she was issued a confirmation hash [%s]", confirmationHash), func() {
+					Convey(fmt.Sprintf("When she confirms her email address with confirmation hash [%s]", confirmationHash), func() {
 						err = commandHandler.ConfirmEmailAddress(confirmEmailAddress)
 						So(err, ShouldBeNil)
 
 						Convey("Then her email address should be confirmed", func() {
-							EmailAddressShouldBeConfirmed(register.CustomerID(), customerEventStore)
-
-							Convey(fmt.Sprintf("When she tries to confirm it again with confirmation hash [%s]", issuedHash), func() {
-								err = commandHandler.ConfirmEmailAddress(confirmEmailAddress)
-
-								Convey("Then it should be ignored", func() {
-									So(err, ShouldBeNil)
-									EmailAddressShouldBeConfirmed(register.CustomerID(), customerEventStore)
-								})
-							})
+							ThenEventStreamShouldBe(
+								events.Registered{},
+								events.EmailAddressConfirmed{},
+							)
 						})
 					})
 				})
@@ -82,28 +75,21 @@ func TestCommandHandlerScenarios(t *testing.T) {
 
 		Convey("\nSCENARIO 3: A Customer fails to confirm her email address", func() {
 			Convey(fmt.Sprintf("Given a Customer registered as [%s %s] with [%s]", givenName, familyName, emailAddress), func() {
-				err := commandHandler.Register(register)
-				So(err, ShouldBeNil)
+				GivenCustomerRegistered()
 
-				Convey(fmt.Sprintf("and she was issued a confirmation hash [%s]", issuedHash), func() {
-					invalidHash := values.GenerateConfirmationHash(register.EmailAddress().EmailAddress()).Hash()
-
-					Convey(fmt.Sprintf("When she tries to confirm her email address with invalid confirmation hash [%s]", invalidHash), func() {
-						confirmEmailAddress, err := commands.NewConfirmEmailAddress(
-							register.CustomerID().ID(),
-							register.EmailAddress().EmailAddress(),
-							invalidHash,
-						)
-						So(err, ShouldBeNil)
-
-						err = commandHandler.ConfirmEmailAddress(confirmEmailAddress)
+				Convey(fmt.Sprintf("and she was issued a confirmation hash [%s]", confirmationHash), func() {
+					Convey(fmt.Sprintf("When she tries to confirm her email address with invalid confirmation hash [%s]", invalidConfirmationHash), func() {
+						err = commandHandler.ConfirmEmailAddress(confirmEmailAddressWithInvalidHash)
+						So(err, ShouldBeError)
 
 						Convey("Then it should fail", func() {
-							So(err, ShouldBeError)
 							So(errors.Is(err, lib.ErrDomainConstraintsViolation), ShouldBeTrue)
 
 							Convey("and her email address should be unconfirmed", func() {
-								EmailAddressShouldNotBeConfirmed(register.CustomerID(), customerEventStore)
+								ThenEventStreamShouldBe(
+									events.Registered{},
+									events.EmailAddressConfirmationFailed{},
+								)
 							})
 						})
 					})
@@ -111,49 +97,23 @@ func TestCommandHandlerScenarios(t *testing.T) {
 			})
 		})
 
-		Convey("\nSCENARIO 4: A Customer changes her confirmed email address", func() {
+		Convey("\nSCENARIO 4: A Customer confirms her email address twice", func() {
 			Convey(fmt.Sprintf("Given a Customer registered as [%s %s] with [%s]", givenName, familyName, emailAddress), func() {
-				err := commandHandler.Register(register)
-				So(err, ShouldBeNil)
+				GivenCustomerRegistered()
 
-				Convey("and she confirmed her email address", func() {
-					confirmEmailAddress, err := commands.NewConfirmEmailAddress(
-						register.CustomerID().ID(),
-						register.EmailAddress().EmailAddress(),
-						register.ConfirmationHash().Hash(),
-					)
-					So(err, ShouldBeNil)
+				Convey(fmt.Sprintf("and she was issued a confirmation hash [%s]", confirmationHash), func() {
+					Convey("and she confirmed her email address", func() {
+						GivenEmailAddressConfirmed(2)
 
-					err = commandHandler.ConfirmEmailAddress(confirmEmailAddress)
-					So(err, ShouldBeNil)
+						Convey(fmt.Sprintf("When she tries to confirm it again with confirmation hash [%s]", confirmationHash), func() {
+							err = commandHandler.ConfirmEmailAddress(confirmEmailAddress)
+							So(err, ShouldBeNil)
 
-					newEmailAddress := "fiona@pratt.net"
-
-					Convey(fmt.Sprintf("When she changes her email address to [%s]", newEmailAddress), func() {
-						changeEmailAddress, err := commands.NewChangeEmailAddress(
-							register.CustomerID().ID(),
-							newEmailAddress,
-						)
-						So(err, ShouldBeNil)
-
-						err = commandHandler.ChangeEmailAddress(changeEmailAddress)
-						So(err, ShouldBeNil)
-
-						Convey("Then her email address should be changed and unconfirmed", func() {
-							MyEmailAddressShouldBeChangedAndUnconfirmed(register.CustomerID(), customerEventStore)
-
-							Convey(fmt.Sprintf("When she tries to change it again to [%s]", newEmailAddress), func() {
-								changeEmailAddress, err := commands.NewChangeEmailAddress(
-									register.CustomerID().ID(),
-									newEmailAddress,
+							Convey("Then it should be ignored", func() {
+								ThenEventStreamShouldBe(
+									events.Registered{},
+									events.EmailAddressConfirmed{},
 								)
-								So(err, ShouldBeNil)
-
-								err = commandHandler.ChangeEmailAddress(changeEmailAddress)
-								Convey("Then it should be ignored", func() {
-									So(err, ShouldBeNil)
-									MyEmailAddressShouldBeChangedAndUnconfirmed(register.CustomerID(), customerEventStore)
-								})
 							})
 						})
 					})
@@ -161,104 +121,212 @@ func TestCommandHandlerScenarios(t *testing.T) {
 			})
 		})
 
-		Convey("\nSCENARIO 5: A Customer confirms her changed email address", func() {
+		Convey("\nSCENARIO 5: A Customer changes her email address", func() {
 			Convey(fmt.Sprintf("Given a Customer registered as [%s %s] with [%s]", givenName, familyName, emailAddress), func() {
-				err := commandHandler.Register(register)
-				So(err, ShouldBeNil)
+				GivenCustomerRegistered()
 
-				Convey("and she confirmed her email address", func() {
-					confirmEmailAddress, err := commands.NewConfirmEmailAddress(
-						register.CustomerID().ID(),
-						register.EmailAddress().EmailAddress(),
-						register.ConfirmationHash().Hash(),
-					)
+				Convey(fmt.Sprintf("When she changes her email address to [%s]", changedEmailAddress), func() {
+					err = commandHandler.ChangeEmailAddress(changeEmailAddress)
 					So(err, ShouldBeNil)
 
-					err = commandHandler.ConfirmEmailAddress(confirmEmailAddress)
-					So(err, ShouldBeNil)
-
-					newEmailAddress := "fiona@pratt.net"
-
-					Convey(fmt.Sprintf("and she changed her email address to [%s]", newEmailAddress), func() {
-						changeEmailAddress, err := commands.NewChangeEmailAddress(
-							register.CustomerID().ID(),
-							newEmailAddress,
+					Convey("Then her email address should be changed", func() {
+						ThenEventStreamShouldBe(
+							events.Registered{},
+							events.EmailAddressChanged{},
 						)
-						So(err, ShouldBeNil)
+					})
+				})
+			})
+		})
 
-						issuedHash := changeEmailAddress.ConfirmationHash().Hash()
+		Convey("\nSCENARIO 6: A Customer changes her email address twice", func() {
+			Convey(fmt.Sprintf("Given a Customer registered as [%s %s] with [%s]", givenName, familyName, emailAddress), func() {
+				GivenCustomerRegistered()
 
+				Convey(fmt.Sprintf("and she changed her email address to [%s]", changedEmailAddress), func() {
+					GivenEmailAddressChanged(2)
+
+					Convey(fmt.Sprintf("When she tries to change it again to [%s]", changedEmailAddress), func() {
 						err = commandHandler.ChangeEmailAddress(changeEmailAddress)
 						So(err, ShouldBeNil)
 
-						Convey(fmt.Sprintf("and she was issued a confirmation hash [%s]", issuedHash), func() {
-							Convey(fmt.Sprintf("When she confirms her changed email address with confirmation hash [%s]", issuedHash), func() {
-								confirmEmailAddress, err := commands.NewConfirmEmailAddress(
-									changeEmailAddress.CustomerID().ID(),
-									changeEmailAddress.EmailAddress().EmailAddress(),
-									issuedHash,
-								)
-								So(err, ShouldBeNil)
+						Convey("Then it should be ignored", func() {
+							ThenEventStreamShouldBe(
+								events.Registered{},
+								events.EmailAddressChanged{},
+							)
+						})
+					})
+				})
+			})
+		})
 
-								err = commandHandler.ConfirmEmailAddress(confirmEmailAddress)
+		Convey("\nSCENARIO 7: A Customer confirms her changed email address", func() {
+			Convey(fmt.Sprintf("Given a Customer registered as [%s %s] with [%s]", givenName, familyName, emailAddress), func() {
+				GivenCustomerRegistered()
+
+				Convey("and she confirmed her email address", func() {
+					GivenEmailAddressConfirmed(2)
+
+					Convey(fmt.Sprintf("and she changed her email address to [%s]", changedEmailAddress), func() {
+						GivenEmailAddressChanged(3)
+
+						Convey(fmt.Sprintf("and she was issued a confirmation hash [%s]", changedConfirmationHash), func() {
+							Convey(fmt.Sprintf("When she confirms her changed email address with confirmation hash [%s]", changedConfirmationHash), func() {
+								err = commandHandler.ConfirmEmailAddress(confirmChangedEmailAddress)
 								So(err, ShouldBeNil)
 
 								Convey("Then her changed email address should be confirmed", func() {
-									MyChangedEmailAddressShouldBeConfirmed(register.CustomerID(), customerEventStore)
+									ThenEventStreamShouldBe(
+										events.Registered{},
+										events.EmailAddressConfirmed{},
+										events.EmailAddressChanged{},
+										events.EmailAddressConfirmed{},
+									)
 								})
 							})
 						})
 					})
 				})
 			})
-
 		})
 
 		Reset(func() {
-			err := customerEventStore.Delete(register.CustomerID())
+			err := customerEventStore.Delete(customerID)
 			So(err, ShouldBeNil)
 		})
 	})
 }
 
-func AccountShouldBeRegisteredAndUnconfirmed(customerID values.CustomerID, customerEventStore *eventstore.CustomerEventStore) {
-	eventStream, err := customerEventStore.EventStreamFor(customerID)
+func GivenCustomerRegistered() {
+	recordedEvents := es.DomainEvents{
+		events.ItWasRegistered(
+			customerID,
+			values.RebuildEmailAddress(emailAddress),
+			values.RebuildConfirmationHash(confirmationHash),
+			values.RebuildPersonName(givenName, familyName),
+			1,
+		),
+	}
+
+	err := customerEventStore.CreateStreamFrom(recordedEvents, customerID)
 	So(err, ShouldBeNil)
-	So(eventStream, ShouldHaveLength, 1)
-	So(eventStream[0], ShouldHaveSameTypeAs, events.Registered{})
 }
 
-func EmailAddressShouldBeConfirmed(customerID values.CustomerID, customerEventStore *eventstore.CustomerEventStore) {
-	eventStream, err := customerEventStore.EventStreamFor(customerID)
+func GivenEmailAddressConfirmed(streamVersion uint) {
+	recordedEvents := es.DomainEvents{
+		events.EmailAddressWasConfirmed(
+			customerID,
+			values.RebuildEmailAddress(emailAddress),
+			streamVersion,
+		),
+	}
+
+	err := customerEventStore.Add(recordedEvents, customerID)
 	So(err, ShouldBeNil)
-	So(eventStream, ShouldHaveLength, 2)
-	So(eventStream[0], ShouldHaveSameTypeAs, events.Registered{})
-	So(eventStream[1], ShouldHaveSameTypeAs, events.EmailAddressConfirmed{})
 }
 
-func EmailAddressShouldNotBeConfirmed(customerID values.CustomerID, customerEventStore *eventstore.CustomerEventStore) {
-	eventStream, err := customerEventStore.EventStreamFor(customerID)
+func GivenEmailAddressChanged(streamVersion uint) {
+	recordedEvents := es.DomainEvents{
+		events.EmailAddressWasChanged(
+			customerID,
+			values.RebuildEmailAddress(changedEmailAddress),
+			values.RebuildConfirmationHash(changedConfirmationHash),
+			streamVersion,
+		),
+	}
+
+	err := customerEventStore.Add(recordedEvents, customerID)
 	So(err, ShouldBeNil)
-	So(eventStream, ShouldHaveLength, 2)
-	So(eventStream[0], ShouldHaveSameTypeAs, events.Registered{})
-	So(eventStream[1], ShouldHaveSameTypeAs, events.EmailAddressConfirmationFailed{})
 }
 
-func MyChangedEmailAddressShouldBeConfirmed(customerID values.CustomerID, customerEventStore *eventstore.CustomerEventStore) {
+func ThenEventStreamShouldBe(domainEvents ...es.DomainEvent) {
 	eventStream, err := customerEventStore.EventStreamFor(customerID)
 	So(err, ShouldBeNil)
-	So(eventStream, ShouldHaveLength, 4)
-	So(eventStream[0], ShouldHaveSameTypeAs, events.Registered{})
-	So(eventStream[1], ShouldHaveSameTypeAs, events.EmailAddressConfirmed{})
-	So(eventStream[2], ShouldHaveSameTypeAs, events.EmailAddressChanged{})
-	So(eventStream[3], ShouldHaveSameTypeAs, events.EmailAddressConfirmed{})
+
+	So(eventStream, ShouldHaveLength, len(domainEvents))
+
+	for idx, event := range domainEvents {
+		So(eventStream[idx], ShouldHaveSameTypeAs, event)
+	}
 }
 
-func MyEmailAddressShouldBeChangedAndUnconfirmed(customerID values.CustomerID, customerEventStore *eventstore.CustomerEventStore) {
-	eventStream, err := customerEventStore.EventStreamFor(customerID)
+func setUpForCommandHandlerScenarios() {
+	var err error
+
+	diContainer := setUpDiContainerForCommandHandlerScenarios()
+	customerEventStore = diContainer.GetCustomerEventStore()
+	commandHandler = diContainer.GetCustomerCommandHandler()
+
+	emailAddress = "fiona@gallagher.net"
+	givenName = "Fiona"
+	familyName = "Gallagher"
+
+	register, err = commands.NewRegister(
+		emailAddress,
+		givenName,
+		familyName,
+	)
 	So(err, ShouldBeNil)
-	So(eventStream, ShouldHaveLength, 3)
-	So(eventStream[0], ShouldHaveSameTypeAs, events.Registered{})
-	So(eventStream[1], ShouldHaveSameTypeAs, events.EmailAddressConfirmed{})
-	So(eventStream[2], ShouldHaveSameTypeAs, events.EmailAddressChanged{})
+
+	customerID = register.CustomerID()
+	confirmationHash = register.ConfirmationHash().Hash()
+
+	confirmEmailAddress, err = commands.NewConfirmEmailAddress(
+		customerID.ID(),
+		emailAddress,
+		confirmationHash,
+	)
+	So(err, ShouldBeNil)
+
+	invalidConfirmationHash = values.GenerateConfirmationHash(emailAddress).Hash()
+
+	confirmEmailAddressWithInvalidHash, err = commands.NewConfirmEmailAddress(
+		customerID.ID(),
+		emailAddress,
+		invalidConfirmationHash,
+	)
+	So(err, ShouldBeNil)
+
+	changedEmailAddress = "fiona@pratt.net"
+
+	changeEmailAddress, err = commands.NewChangeEmailAddress(
+		customerID.ID(),
+		changedEmailAddress,
+	)
+	So(err, ShouldBeNil)
+
+	changedConfirmationHash = changeEmailAddress.ConfirmationHash().Hash()
+
+	confirmChangedEmailAddress, err = commands.NewConfirmEmailAddress(
+		customerID.ID(),
+		changedEmailAddress,
+		changedConfirmationHash,
+	)
+	So(err, ShouldBeNil)
+}
+
+func setUpDiContainerForCommandHandlerScenarios() *cmd.DIContainer {
+	config, err := cmd.NewConfigFromEnv()
+	So(err, ShouldBeNil)
+
+	db, err := sql.Open("postgres", config.Postgres.DSN)
+	So(err, ShouldBeNil)
+
+	err = db.Ping()
+	So(err, ShouldBeNil)
+
+	migrator, err := database.NewMigrator(db, config.Postgres.MigrationsPath)
+	So(err, ShouldBeNil)
+
+	err = migrator.Up()
+	So(err, ShouldBeNil)
+
+	diContainer, err := cmd.NewDIContainer(
+		db,
+		events.UnmarshalCustomerEvent,
+	)
+	So(err, ShouldBeNil)
+
+	return diContainer
 }
