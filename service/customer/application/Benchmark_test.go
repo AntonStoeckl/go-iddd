5015b9f2f87096ae9741997c2886c9ca5ec7c6c8
anton <anton@stoeckl.de>
2020-03-22 01:07:12 +0100

    Move test artifacts into a struct

--- service/customer/application/Benchmark_test.go
+++ service/customer/application/Benchmark_test.go
@@ -3,18 +3,54 @@ package application_test
 import (
 	"testing"
 
+	"github.com/AntonStoeckl/go-iddd/service/cmd"
+	"github.com/AntonStoeckl/go-iddd/service/customer/application/command"
+	"github.com/AntonStoeckl/go-iddd/service/customer/domain/customer/commands"
 	"github.com/AntonStoeckl/go-iddd/service/customer/domain/customer/values"
 	"github.com/AntonStoeckl/go-iddd/service/customer/infrastructure/secondary/eventstore"
-
-	"github.com/AntonStoeckl/go-iddd/service/customer/application/command"
-
-	"github.com/AntonStoeckl/go-iddd/service/cmd"
-	"github.com/AntonStoeckl/go-iddd/service/customer/domain/customer/commands"
 )
 
-func BenchmarkCustomerScenario(b *testing.B) {
-	var err error
+type benchmarkTestArtifacts struct {
+	registerCustomer               commands.RegisterCustomer
+	confirmCustomerEmailAddress    commands.ConfirmCustomerEmailAddress
+	changeCustomerEmailAddress     commands.ChangeCustomerEmailAddress
+	changeCustomerEmailAddressBack commands.ChangeCustomerEmailAddress
+	changeCustomerName             commands.ChangeCustomerName
+	changeCustomerNameBack         commands.ChangeCustomerName
+}
 
+func BenchmarkCustomerCommand(b *testing.B) {
+	diContainer, err := cmd.Bootstrap()
+	if err != nil {
+		panic(err)
+	}
+
+	commandHandler := diContainer.GetCustomerCommandHandler()
+	ba := buildArtifactsForBenchmarkTest(b)
+	prepareForBenchmark(b, commandHandler, ba)
+
+	b.Run("ChangeName", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			if n%2 == 0 {
+				if err = commandHandler.ChangeCustomerName(ba.changeCustomerName); err != nil {
+					b.FailNow()
+				}
+			} else {
+				if err = commandHandler.ChangeCustomerName(ba.changeCustomerNameBack); err != nil {
+					b.FailNow()
+				}
+			}
+		}
+	})
+
+	cleanUpAfterBenchmark(
+		b,
+		diContainer.GetCustomerEventStore(),
+		ba.registerCustomer.CustomerID(),
+	)
+}
+
+func BenchmarkCustomerQuery(b *testing.B) {
 	diContainer, err := cmd.Bootstrap()
 	if err != nil {
 		panic(err)
@@ -22,100 +58,12 @@ func BenchmarkCustomerScenario(b *testing.B) {
 
 	commandHandler := diContainer.GetCustomerCommandHandler()
 	queryHandler := diContainer.GetCustomerQueryHandler()
-
-	emailAddress := "fiona@gallagher.net"
-	givenName := "Fiona"
-	familyName := "Galagher"
-	newEmailAddress := "fiona@pratt.net"
-	newGivenName := "Fiona"
-	newFamilyName := "Pratt"
-
-	registerCustomer, err := commands.BuildRegisterCustomer(
-		emailAddress,
-		givenName,
-		familyName,
-	)
-
-	if err != nil {
-		b.FailNow()
-	}
-
-	confirmCustomerEmailAddress, err := commands.BuildConfirmCustomerEmailAddress(
-		registerCustomer.CustomerID().ID(),
-		registerCustomer.ConfirmationHash().Hash(),
-	)
-
-	if err != nil {
-		b.FailNow()
-	}
-
-	changeCustomerEmailAddress, err := commands.BuildChangeCustomerEmailAddress(
-		registerCustomer.CustomerID().ID(),
-		newEmailAddress,
-	)
-
-	if err != nil {
-		b.FailNow()
-	}
-
-	changeCustomerEmailAddressBack, err := commands.BuildChangeCustomerEmailAddress(
-		registerCustomer.CustomerID().ID(),
-		emailAddress,
-	)
-
-	if err != nil {
-		b.FailNow()
-	}
-
-	changeCustomerName, err := commands.BuildChangeCustomerName(
-		registerCustomer.CustomerID().ID(),
-		newGivenName,
-		newFamilyName,
-	)
-
-	if err != nil {
-		b.FailNow()
-	}
-
-	changeCustomerNameBack, err := commands.BuildChangeCustomerName(
-		registerCustomer.CustomerID().ID(),
-		givenName,
-		familyName,
-	)
-
-	if err != nil {
-		b.FailNow()
-	}
-
-	prepareForBenchmark(b,
-		commandHandler,
-		registerCustomer,
-		confirmCustomerEmailAddress,
-		changeCustomerEmailAddress,
-		changeCustomerEmailAddressBack,
-	)
-
-	/**************************************************/
-
-	b.Run("ChangeName", func(b *testing.B) {
-		for n := 0; n < b.N; n++ {
-			if n%2 == 0 {
-				if err = commandHandler.ChangeCustomerName(changeCustomerName); err != nil {
-					b.FailNow()
-				}
-			} else {
-				if err = commandHandler.ChangeCustomerName(changeCustomerNameBack); err != nil {
-					b.FailNow()
-				}
-			}
-		}
-	})
-
-	/**************************************************/
+	ba := buildArtifactsForBenchmarkTest(b)
+	prepareForBenchmark(b, commandHandler, ba)
 
 	b.Run("CustomerViewByID", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
-			if _, err = queryHandler.CustomerViewByID(registerCustomer.CustomerID()); err != nil {
+			if _, err := queryHandler.CustomerViewByID(ba.registerCustomer.CustomerID()); err != nil {
 				b.FailNow()
 			}
 		}
@@ -124,36 +72,104 @@ func BenchmarkCustomerScenario(b *testing.B) {
 	cleanUpAfterBenchmark(
 		b,
 		diContainer.GetCustomerEventStore(),
-		registerCustomer.CustomerID(),
+		ba.registerCustomer.CustomerID(),
 	)
+}
+
+func buildArtifactsForBenchmarkTest(b *testing.B) benchmarkTestArtifacts {
+	var err error
+	var ba benchmarkTestArtifacts
+
+	emailAddress := "fiona@gallagher.net"
+	givenName := "Fiona"
+	familyName := "Galagher"
+	newEmailAddress := "fiona@pratt.net"
+	newGivenName := "Fiona"
+	newFamilyName := "Pratt"
+
+	ba.registerCustomer, err = commands.BuildRegisterCustomer(
+		emailAddress,
+		givenName,
+		familyName,
+	)
+
+	if err != nil {
+		b.FailNow()
+	}
+
+	ba.confirmCustomerEmailAddress, err = commands.BuildConfirmCustomerEmailAddress(
+		ba.registerCustomer.CustomerID().ID(),
+		ba.registerCustomer.ConfirmationHash().Hash(),
+	)
+
+	if err != nil {
+		b.FailNow()
+	}
+
+	ba.changeCustomerEmailAddress, err = commands.BuildChangeCustomerEmailAddress(
+		ba.registerCustomer.CustomerID().ID(),
+		newEmailAddress,
+	)
+
+	if err != nil {
+		b.FailNow()
+	}
+
+	ba.changeCustomerEmailAddressBack, err = commands.BuildChangeCustomerEmailAddress(
+		ba.registerCustomer.CustomerID().ID(),
+		emailAddress,
+	)
+
+	if err != nil {
+		b.FailNow()
+	}
+
+	ba.changeCustomerName, err = commands.BuildChangeCustomerName(
+		ba.registerCustomer.CustomerID().ID(),
+		newGivenName,
+		newFamilyName,
+	)
+
+	if err != nil {
+		b.FailNow()
+	}
+
+	ba.changeCustomerNameBack, err = commands.BuildChangeCustomerName(
+		ba.registerCustomer.CustomerID().ID(),
+		givenName,
+		familyName,
+	)
+
+	if err != nil {
+		b.FailNow()
+	}
+
+	return ba
 }
 
 func prepareForBenchmark(
 	b *testing.B,
 	commandHandler *command.CustomerCommandHandler,
-	registerCustomer commands.RegisterCustomer,
-	confirmCustomerEmailAddress commands.ConfirmCustomerEmailAddress,
-	changeCustomerEmailAddress commands.ChangeCustomerEmailAddress,
-	changeCustomerEmailAddressBack commands.ChangeCustomerEmailAddress,
+	ba benchmarkTestArtifacts,
 ) {
 
 	var err error
 
-	if err = commandHandler.RegisterCustomer(registerCustomer); err != nil {
+	if err = commandHandler.RegisterCustomer(ba.registerCustomer); err != nil {
 		b.FailNow()
 	}
 
-	if err = commandHandler.ConfirmCustomerEmailAddress(confirmCustomerEmailAddress); err != nil {
+	if err = commandHandler.ConfirmCustomerEmailAddress(ba.confirmCustomerEmailAddress); err != nil {
 		b.FailNow()
 	}
 
 	for n := 0; n < 100; n++ {
 		if n%2 == 0 {
-			if err = commandHandler.ChangeCustomerEmailAddress(changeCustomerEmailAddress); err != nil {
+			if err = commandHandler.ChangeCustomerEmailAddress(ba.changeCustomerEmailAddress); err != nil {
 				b.FailNow()
 			}
 		} else {
-			if err = commandHandler.ChangeCustomerEmailAddress(changeCustomerEmailAddressBack); err != nil {
+			if err = commandHandler.ChangeCustomerEmailAddress(ba.changeCustomerEmailAddressBack); err != nil {
 				b.FailNow()
 			}
 		}
