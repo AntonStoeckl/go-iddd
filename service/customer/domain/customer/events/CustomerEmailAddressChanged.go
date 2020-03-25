5b46885ea44f0ebdf7b7c04973e2ee6b30796cdf
Anton Stöckl <anton.stoeckl@maibornwolff.de>
2020-03-25 16:22:24 +0100

    Add previewEmailAddress to marshaling and unmarshaling

--- service/customer/domain/customer/events/CustomerEmailAddressChanged.go
+++ service/customer/domain/customer/events/CustomerEmailAddressChanged.go
@@ -18,7 +18,7 @@ type CustomerEmailAddressChangedForJSON struct {
 	CustomerID           string    `json:"customerID"`
 	EmailAddress         string    `json:"emailAddress"`
 	ConfirmationHash     string    `json:"confirmationHash"`
-	PreviousEmailAddress string    `json:"previousEailAddress"`
+	PreviousEmailAddress string    `json:"previousEmailAddress"`
 	Meta                 EventMeta `json:"meta"`
 }
 
@@ -76,10 +76,11 @@ func (event CustomerEmailAddressChanged) StreamVersion() uint {
 
 func (event CustomerEmailAddressChanged) MarshalJSON() ([]byte, error) {
 	data := CustomerEmailAddressChangedForJSON{
-		CustomerID:       event.customerID.ID(),
-		EmailAddress:     event.emailAddress.EmailAddress(),
-		ConfirmationHash: event.confirmationHash.Hash(),
-		Meta:             event.meta,
+		CustomerID:           event.customerID.ID(),
+		EmailAddress:         event.emailAddress.EmailAddress(),
+		ConfirmationHash:     event.confirmationHash.Hash(),
+		PreviousEmailAddress: event.previousEmailAddress.EmailAddress(),
+		Meta:                 event.meta,
 	}
 
 	return jsoniter.ConfigFastest.Marshal(data)
@@ -95,10 +96,11 @@ func UnmarshalCustomerEmailAddressChangedFromJSON(
 	_ = jsoniter.ConfigFastest.Unmarshal(data, unmarshaledData)
 
 	event := CustomerEmailAddressChanged{
-		customerID:       values.RebuildCustomerID(unmarshaledData.CustomerID),
-		emailAddress:     values.RebuildEmailAddress(unmarshaledData.EmailAddress),
-		confirmationHash: values.RebuildConfirmationHash(unmarshaledData.ConfirmationHash),
-		meta:             EnrichEventMeta(unmarshaledData.Meta, streamVersion),
+		customerID:           values.RebuildCustomerID(unmarshaledData.CustomerID),
+		emailAddress:         values.RebuildEmailAddress(unmarshaledData.EmailAddress),
+		confirmationHash:     values.RebuildConfirmationHash(unmarshaledData.ConfirmationHash),
+		previousEmailAddress: values.RebuildEmailAddress(unmarshaledData.PreviousEmailAddress),
+		meta:                 EnrichEventMeta(unmarshaledData.Meta, streamVersion),
 	}
 
 	return event
