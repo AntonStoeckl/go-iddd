5c1acdd558fc16d114ccb76a2c4085366c86ca8e
Anton Stöckl <anton.stoeckl@maibornwolff.de>
2020-04-14 14:53:31 +0200

    Add test case

--- service/cmd/DIContainer_test.go
+++ service/cmd/DIContainer_test.go
@@ -4,13 +4,17 @@ import (
 	"database/sql"
 	"testing"
 
+	"github.com/AntonStoeckl/go-iddd/service/lib"
+	"github.com/cockroachdb/errors"
+
 	"github.com/AntonStoeckl/go-iddd/service/lib/es"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestNewDIContainer(t *testing.T) {
-	Convey("When a DIContainer is created with a nil postgresDBConn", t, func() {
-		var conn *sql.DB
+	Convey("When a DIContainer is created with a valid postgres DB connection", t, func() {
+		db, err := sql.Open("postgres", "postgresql://test:test@localhost:15432/test?sslmode=disable")
+		So(err, ShouldBeNil)
 
 		marshalDomainEvent := func(event es.DomainEvent) ([]byte, error) {
 			return nil, nil
@@ -20,10 +24,33 @@ func TestNewDIContainer(t *testing.T) {
 			return nil, nil
 		}
 
-		_, err := NewDIContainer(conn, marshalDomainEvent, unmarshalDomainEvent)
+		diContainer, err := NewDIContainer(db, marshalDomainEvent, unmarshalDomainEvent)
+
+		Convey("Then it should succeed", func() {
+			So(err, ShouldBeNil)
+
+			Convey("And it should expose the postgres DB connection", func() {
+				So(diContainer.GetPostgresDBConn(), ShouldResemble, db)
+			})
+		})
+	})
+
+	Convey("When a DIContainer is created with a nil postgres DB connection", t, func() {
+		var db *sql.DB
+
+		marshalDomainEvent := func(event es.DomainEvent) ([]byte, error) {
+			return nil, nil
+		}
+
+		unmarshalDomainEvent := func(name string, payload []byte, streamVersion uint) (es.DomainEvent, error) {
+			return nil, nil
+		}
+
+		_, err := NewDIContainer(db, marshalDomainEvent, unmarshalDomainEvent)
 
 		Convey("Then it should fail", func() {
 			So(err, ShouldBeError)
+			So(errors.Is(err, lib.ErrTechnical), ShouldBeTrue)
 		})
 	})
 }
