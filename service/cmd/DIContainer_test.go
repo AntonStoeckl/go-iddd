b71ed77096c077a3b3861023a2817370505cd925
anton <anton@stoeckl.de>
2020-12-13 22:02:41 +0100

    Simplify building the lambda functions passed to container factory method

--- service/cmd/DIContainer_test.go
+++ service/cmd/DIContainer_test.go
@@ -12,22 +12,26 @@ import (
 )
 
 func TestNewDIContainer(t *testing.T) {
+	marshalDomainEvent := func(event es.DomainEvent) ([]byte, error) {
+		return nil, nil
+	}
+
+	unmarshalDomainEvent := func(name string, payload []byte, streamVersion uint) (es.DomainEvent, error) {
+		return nil, nil
+	}
+
+	buildUniqueEmailAddressAssertions := func(recordedEvents ...es.DomainEvent) customer.UniqueEmailAddressAssertions {
+		return nil
+	}
+
 	Convey("When a DIContainer is created with valid input", t, func() {
 		db, err := sql.Open("postgres", "postgresql://test:test@localhost:15432/test?sslmode=disable")
 		So(err, ShouldBeNil)
 
-		marshalDomainEvent := func(event es.DomainEvent) ([]byte, error) {
-			return nil, nil
-		}
-
-		unmarshalDomainEvent := func(name string, payload []byte, streamVersion uint) (es.DomainEvent, error) {
-			return nil, nil
-		}
-
 		diContainer, err := NewDIContainer(
 			marshalDomainEvent,
 			unmarshalDomainEvent,
-			customer.BuildUniqueEmailAddressAssertions,
+			buildUniqueEmailAddressAssertions,
 			WithPostgresDBConn(db),
 		)
 
@@ -44,9 +48,9 @@ func TestNewDIContainer(t *testing.T) {
 		var db *sql.DB
 
 		_, err := NewDIContainer(
-			func(event es.DomainEvent) ([]byte, error) { return nil, nil },
-			func(name string, payload []byte, streamVersion uint) (es.DomainEvent, error) { return nil, nil },
-			func(recordedEvents ...es.DomainEvent) customer.UniqueEmailAddressAssertions { return nil },
+			marshalDomainEvent,
+			unmarshalDomainEvent,
+			buildUniqueEmailAddressAssertions,
 			WithPostgresDBConn(db),
 		)
 
